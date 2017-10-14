a6cf309ded6fea5bda6cd9c68a04ef37f006d223
alxbrd <alex+git@onboot.org>
2017-10-14 16:22:43 +0100

    Added deploy script

---
+++ deploy/github_deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git clone -b gh-pages git@github.com:mde-optimiser/mde-optimiser.github.io.git
+
+rm -rf gh-pages/*
+
+cp ../src/_site/* ./
+
+git add --all
+
+git commit -m "MDEO Website updated"
+
+git push origin gh-pages
+
+cd ..
+
+rm -rf gh-pages
+
+echo "Deployment completed"
+
+
